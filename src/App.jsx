d7cb4444a0f69613212230e45e7c897bee8e3a23
LangerTwo <adrangel95@gmail.com>
2024-11-06 16:48:18 -0500

    ya limpio la pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useFetch } from './useFetch';
 import Filter from './Filter';
 import Card from './Card';
@@ -6,9 +6,17 @@ import './App.css';
 
 function App() {
   const [generationUrl, setGenerationUrl] = useState(null);
-  const { data, loading, error } = useFetch(generationUrl);
+  const [data, setData] = useState(null);
+  const { data: fetchedData, loading, error } = useFetch(generationUrl);
+
+  // Actualiza los datos cuando `fetchedData` cambia
+  useEffect(() => {
+    setData(fetchedData);
+  }, [fetchedData]);
 
   const handleCategoryChange = (category, option) => {
+    // Limpiar datos antes de cambiar la URL
+    setData(null);
     if (category === 'region' && option) {
       setGenerationUrl(`https://pokeapi.co/api/v2/generation/${option}`);
     } else if (category === 'type' && option) {
@@ -25,7 +33,7 @@ function App() {
         <div className="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
           {error && <div>Error: {error}</div>}
           {loading && <div>Loading...</div>}
-          <Card data={data} /> {/* Pasar data como prop */}
+          {data && <Card data={data} />} {/* Renderizar solo si hay datos */}
         </div>
       )}
     </>
