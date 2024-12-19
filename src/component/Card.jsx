4b8d7d088daa2b92b421acf581ac7d4f7e1d650f
LangerTwo <adrangel95@gmail.com>
2024-12-19 15:59:11 -0500

    arreglando la url para ver region

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 function Card({ filteredList }) { // Recibe data como prop
+
+    const { regionName } = useParams();
+    const [generationUrl, setGenerationUrl] = useState(``);
+
+    useEffect(() => {
+            if (regionName) {
+                // console.log("RegionName:", regionName);
+                // console.log("URL Param (regionName):", regionName);
+              // Convierte el nombre de la región a minúsculas para coincidir con la API
+              const regionIdMap = {
+                kanto: 1,
+                johto: 2,
+                hoenn: 3,
+                sinnoh: 4,
+                unova: 5,
+                kalos: 6,
+                alola: 7,
+                galar: 8
+              };
+              const regionId = regionIdMap[regionName.toLowerCase()];
+              if (regionId) {
+                setGenerationUrl(`https://pokeapi.co/api/v2/generation/${regionId}`);
+              }
+            }
+        }, [regionName]);
+
     return (
         <>
             {filteredList?.map((poke) => (
@@ -22,7 +48,7 @@ function Card({ filteredList }) { // Recibe data como prop
                         <span>No image available</span>
                     )}
                     <div className="p-4 text-center">
-                        <Link to={`/region/kanto/lista-pokemon/pokemon/${poke.name}`}>
+                        <Link to={`/region/${regionName?.toLowerCase() || 'unknown'}/lista-pokemon/pokemon/${poke.name}`}>
                             <div className='mb-2'>
                                     <h3 className='hover:underline'>{poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}</h3>                             
                             </div> 
