fdf0e186e547fb44997268bdfedfcd08332b7e83
LangerTwo <adrangel95@gmail.com>
2025-01-10 16:28:19 -0500

    estilos card regiones hecho

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import teseliaImage from './assets/teselia-region.png';
 import kalosImage from './assets/kalos-region.png';
 import alolaImage from './assets/alola-region.png';
 import galarImage from './assets/galar-region.png';
+import { X } from 'lucide-react';
 
 const regions = [
   { id: 'kanto',
@@ -108,30 +109,88 @@ function Home() {
   }, [])
 
   return (
-    <div className="container mx-auto px-4 py-8 pt-28">
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-        {regions.map((region) => (
-          <div key={region.id} 
-            className={`transform transition-all duration-500 ${animatedRegions.includes(region.id) ? 'translate-y-0 opacity-100' : 'translate-y-4 opacity-0'}`} 
-            // onClick={() => handleRegionClick(region.name)}
-          >
-            <div onClick={() => setSelectedRegion(region)} className="group rounded-lg overflow-hidden hover:scale-105 transition-transform duration-300 cursor-pointer bg-gray-800 border border-gray-700">
-              <div className='aspect-video overflow-hidden'>
-                <img 
-                  src={region.image} 
-                  alt={`Mapa de la región ${region.name}`} 
-                  className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500" 
-                />
+    <>
+      <main className="container mx-auto px-4 py-8 pt-28">
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+          {regions.map((region) => (
+            <div key={region.id} 
+              className={`transform transition-all duration-500 ${animatedRegions.includes(region.id) ? 'translate-y-0 opacity-100' : 'translate-y-4 opacity-0'}`} 
+              // onClick={() => handleRegionClick(region.name)}
+            >
+              <div onClick={() => setSelectedRegion(region)} className="group rounded-lg overflow-hidden hover:scale-105 transition-transform duration-300 cursor-pointer bg-gray-800 border border-gray-700">
+                <div className='aspect-video overflow-hidden'>
+                  <img 
+                    src={region.image} 
+                    alt={`Mapa de la región ${region.name}`} 
+                    className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500" 
+                  />
+                </div>
+                <div className='p-6'>
+                  <h2 className='text-gray-400 text-xl font-semibold mb-2'>{region.name}</h2>
+                  <p className='text-gray-400 line-clamp-2'>{region.description}</p>
+                </div>
               </div>
-              <div className='p-6'>
-                <h2 className='text-gray-400 text-xl font-semibold mb-2'>{region.name}</h2>
-                <p className='text-gray-400 line-clamp-2'>{region.description}</p>
+            </div>
+          ))}
+        </div>
+      </main>
+
+      {/* Modal */}
+      {selectedRegion && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black/50 backdrop-blur-sm">
+          <div className="relative w-full max-w-2xl bg-gray-800 rounded-lg shadow-xl my-8">
+            <div className="p-6 max-h-[100vh] overflow-y-auto">
+              <button
+                onClick={() => setSelectedRegion(null)}
+                className="absolute top-4 right-4 text-gray-400 hover:text-white"
+              >
+                <X className="w-6 h-6" />
+                <span className="sr-only">Cerrar</span>
+              </button>
+              
+              <h2 className="text-gray-300 text-2xl font-bold mb-2">{selectedRegion.name}</h2>
+              <p className="text-gray-400 mb-4">{selectedRegion.description}</p>
+              
+              <img
+                src={selectedRegion.image}
+                alt={`Mapa de la región ${selectedRegion.name}`}
+                className="w-full rounded-lg mb-4"
+              />
+              
+              <div className="space-y-4">
+                <div>
+                  <h3 className="text-gray-300 font-semibold mb-1">Profesor:</h3>
+                  <p className="text-gray-400">{selectedRegion.professor}</p>
+                </div>
+                
+                <div>
+                  <h3 className="text-gray-300 font-semibold mb-1">Pokémon Iniciales:</h3>
+                  <p className="text-gray-400">{selectedRegion.starterPokemons.join(', ')}</p>
+                </div>
+                
+                <div>
+                  <h3 className="text-gray-300 font-semibold mb-1">Villanos:</h3>
+                  <p className="text-gray-400">{selectedRegion.villains}</p>
+                </div>
+                
+                <div>
+                  <h3 className="text-gray-300 font-semibold mb-1">Pokémon Legendarios:</h3>
+                  <p className="text-gray-400">{selectedRegion.legendaryPokemons.join(', ')}</p>
+                </div>
+              </div>
+              <div className="mt-4">
+                <button
+                  onClick={() => handleRegionClick(selectedRegion.name)}
+                  className="w-full bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded transition-colors duration-300 flex items-center justify-center"
+                >
+                  Ver Más <span className="mx-2">→</span>
+                </button>
               </div>
             </div>
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 }
 
