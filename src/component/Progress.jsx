2b5b76f2b8b118d80f1b3e2ce336fbfb61532059
LangerTwo <adrangel95@gmail.com>
2025-01-10 17:22:02 -0500

    arreglando estilos pokemonDetails, aun falta

--- src/component/Progress.jsx
+++ src/component/Progress.jsx
@@ -1,5 +1,5 @@
 function Progress({ value, max, className, indicatorClassName }) {
-    const percentage = Math.min((value / max) * 100, 100); // Limitar al 100%
+    const percentage = Math.min((value / max) * 100, 120); // Limitar al 100%
   
     return (
       <div className={`w-full bg-gray-200 rounded-full ${className}`}>
