9d9ac6ec29f293bd8a468c0ef8c5ec9cbf15e938
LangerTwo <adrangel95@gmail.com>
2025-01-13 15:59:09 -0500

    otros cambios mas

--- src/page/PokemonDetails.jsx
+++ src/page/PokemonDetails.jsx
@@ -87,7 +87,7 @@ function PokemonDetails() {
   if (error) return <div>{error}</div>;
 
   const getStatColor = (value) => {
-    if (value > 100) return "bg-green-500"
+    if (value >= 100) return "bg-green-500"
     if (value > 80) return "bg-yellow-500"
     if (value > 40) return "bg-orange-500"
     if (value > 20) return "bg-yellow-500"
