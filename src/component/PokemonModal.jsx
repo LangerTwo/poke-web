ae88137f4d16a1e7b63b34607b0a21b81fc84543
LangerTwo <adrangel95@gmail.com>
2025-04-01 16:40:01 -0500

    cambio de tamaño al modal

--- src/component/PokemonModal.jsx
+++ src/component/PokemonModal.jsx
@@ -23,7 +23,7 @@ function PokemonModal() {
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-            <div className="bg-white rounded-lg p-6 shadow-lg max-w-md w-full relative">
+            <div className="bg-white rounded-lg p-6 shadow-lg max-w-2xl w-full relative">
                 <button
                     onClick={closeModal}
                     className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
@@ -43,24 +43,8 @@ function PokemonModal() {
                     </div>
                 </>
                 ) : (
-                <MegaEvolutions megaEvolutions={megaEvolutions} />
+                    <MegaEvolutions megaEvolutions={megaEvolutions} />
                 )}
-
-                {/* <h2 className="text-2xl font-bold text-center mb-4">{pokemon.name.toUpperCase()}</h2>
-                
-                <div className="flex justify-center">
-                    <img 
-                        src={pokemon.sprites.other["official-artwork"].front_default} 
-                        alt={pokemon.name} 
-                        className="w-32 h-32"
-                    />
-                </div>
-
-                <div className="mt-4">
-                    <p><strong>Altura:</strong> {pokemon.height / 10} m</p>
-                    <p><strong>Peso:</strong> {pokemon.weight / 10} kg</p>
-                    <p><strong>Tipo:</strong> {pokemon.types.map(t => t.type.name).join(", ")}</p>
-                </div> */}
             </div>
         </div>
     );
